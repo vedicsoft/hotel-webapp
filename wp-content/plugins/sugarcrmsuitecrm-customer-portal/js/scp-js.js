a371c5f70734a524d1e1622d60c16cb8826a284f
parimal786 <er.parimalsharma@gmail.com>
2016-08-17 15:19:04 -0400

    without plugin

--- wp-content/plugins/sugarcrmsuitecrm-customer-portal/js/scp-js.js
+++
@@ -1,7 +0,0 @@
-var pathname = window.location.pathname; // Returns path only
-var url      = window.location.href;     // Returns full URL
-if(pathname=="/feedback-portal/"){
-jQuery(document).ready(function() {
-	jQuery('#demo').DataTable();    
-} );
-}
